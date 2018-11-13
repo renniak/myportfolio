225e9ac5972b06d6ea35e5b6b402b12a108eff32
James <renniak7@gmail.com>
2018-11-13 10:19:30 -0300

    Updates

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ particlesJS("particles-js", {
       anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
     },
     size: {
-      value: 2,
+      value: 1,
       random: true,
       anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
     },
@@ -41,7 +41,7 @@ particlesJS("particles-js", {
     detect_on: "window",
     events: {
       onhover: { enable: true, mode: "repulse" },
-      onclick: { enable: true, mode: "push" },
+      onclick: { enable: false, mode: "push" },
       resize: true
     },
     modes: {
